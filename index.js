c7d1a7efaa3c9b8f885a7d3eaf67b100e1b100d8
mark <markpackham1@gmail.com>
2024-04-24 12:53:03 +0100

    Demo default & regular export
    https://youtu.be/32M1al-Y6Ag?si=Hn6anZc6lTSk_qg6&t=1639

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 // console.log(`Celcius To Fahrenheit: ${celciusToFahrenheit(0)}`);
 
-import getPosts from "./postController.js";
+import getPosts, { getPostsLength } from "./postController.js";
 
 console.log(getPosts());
+
+console.log(`Posts Length: ${getPostsLength()}`);
