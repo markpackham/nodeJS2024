78c0a3f405898de8b99236501cd187e89bcf1d9c
mark <markpackham1@gmail.com>
2024-04-25 13:52:33 +0100

    Demo use of .env file
    https://youtu.be/32M1al-Y6Ag?si=LAY3DwN6UuRMFniD&t=2645

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // Express uses this module
 import http from "http";
 
-const PORT = 8888;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   // res.setHeader("Content-Type", "text/html");
   // res.statusCode = 404;
   // res.write("Hello World");
 
-  res.writeHead(500, { "Content-Type": "application/json" });
+  res.writeHead(200, { "Content-Type": "text/html" });
 
   // If I used Express it'd do the end() part automatically for me
-  res.end(JSON.stringify({ message: "Server Error" }));
+  res.end("<h1>Hello World</h1>");
 });
 
 server.listen(PORT, () => {
