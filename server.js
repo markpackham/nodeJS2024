ee4442b4e7a0f5ad661324bf5596f6fb6bf383c5
mark <markpackham1@gmail.com>
2024-04-25 14:11:44 +0100

    Add basic server routing functionality
    https://youtu.be/32M1al-Y6Ag?si=PTQoUyJKbO0tb18b&t=3130

--- server.js
+++ server.js
@@ -7,11 +7,31 @@ const server = http.createServer((req, res) => {
   // res.setHeader("Content-Type", "text/html");
   // res.statusCode = 404;
   // res.write("Hello World");
+  // console.log(req.url);
+  // console.log(req.method);
 
-  res.writeHead(200, { "Content-Type": "text/html" });
+  try {
+    // Check if GET request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
 
-  // If I used Express it'd do the end() part automatically for me
-  res.end("<h1>Hello World</h1>");
+        // If I used Express it'd do the end() part automatically for me
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("<h1>Server Error</h1>");
+  }
 });
 
 server.listen(PORT, () => {
