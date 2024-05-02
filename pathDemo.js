768344a9a0210c588aa9cd23e7ea20e621c3868a
mark <markpackham1@gmail.com>
2024-05-02 15:48:09 +0100

    Demo __filename & __dirname
    https://youtu.be/32M1al-Y6Ag?si=Ezf4z0_teBHX7yLA&t=5911

--- pathDemo.js
+++ pathDemo.js
@@ -1,4 +1,5 @@
 import path from "path";
+import url from "url";
 
 // Not a real location
 const filePath = "./dir1/dir2/text.txt";
@@ -11,3 +12,18 @@ console.log(path.dirname(filePath));
 
 // extname() the extension name
 console.log(path.extname(filePath));
+
+// parse()
+// {
+//     root: '',
+//     dir: './dir1/dir2',
+//     base: 'text.txt',
+//     ext: '.txt',
+//     name: 'text'
+//   }
+console.log(path.parse(filePath));
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename);
+console.log(__dirname);
