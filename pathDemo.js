82b4cfb37bd24a1f53f6d002ab7980e42f07c76d
mark <markpackham1@gmail.com>
2024-05-02 15:56:16 +0100

    Demo resolve() & join()
    https://youtu.be/32M1al-Y6Ag?si=b5dHdHWLAOqQkiCw&t=6014

--- pathDemo.js
+++ pathDemo.js
@@ -27,3 +27,17 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__filename);
 console.log(__dirname);
+
+// join() handy for dealing with path differences between Linux Vs Windows
+// Windows use \ as a separator while Linux uses /
+// E:\Sites\nodeJS2024\dir1\dir2\test.txt
+const filePath2 = path.join(__dirname, "dir1", "dir2", "test.txt");
+console.log(filePath2);
+
+// resolve()
+// pretty much does the same as join() but join() may or may not return an absolute path
+//  resolve() always returns an absolute path,
+// using the target operating system’s root as the root or first argument with a leading / as the new root
+// https://hackernoon.com/whats-the-difference-between-pathjoin-and-pathresolve
+const filePath3 = path.resolve(__dirname, "dir1", "dir2", "test.txt");
+console.log(filePath3);
